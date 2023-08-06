2d7adce7446929e167c0c0ddf6b20db7bd126d98
DanikingRD <danikingrd@gmail.com>
2023-08-05 21:42:19 -0400

    Add docs & some improvements

--- main.cpp
+++ main.cpp
@@ -1,80 +1,55 @@
 #include <iostream>
-// Not recommended, but this is a very small program.
+
 using namespace std;
 
+/// @brief La cantidad de dígitos que contiene la clave.
 const uint KEY_LENGTH = 4;
+/// @brief El número mas pequeño del rango de números de la clave.
 const uint KEY_RANGE_MIN = 1;
+/// @brief El número más grande del rango de números de la clave.
 const uint KEY_RANGE_MAX = 6;
+/// @brief La cantidad máxima de intentos posibles para adivinar la clave.
 const uint MAX_ATTEMPTS = 10;
 
-// Properly document visual representation of the table.
-
-/*
-    Representa una tabla con los intentos del usuario y la clave secreta.
-    - La primera fila es la clave secreta.
-    - Cada fila representa un intento del usuario.
-
-        Representacion visual:
-
-        -   +---+---+---+---+
-        -   | 5 | 2 | 6 | 1 |
-        -   +---+---+---+---+
-        -   | 1 | 2 | 3 | 4 |
-        -   +---+---+---+---+
-        -   | 1 | 2 | 3 | 4 |
-        -   +---+---+---+---+
-        -   | 1 | 2 | 3 | 4 |
-        -   +---+---+---+---+
-*/
+/**
+ *  Representa una tabla con los intentos del usuario y la clave secreta,
+ *  haciendo uso de un arreglo bidimensional (2D) de enteros.
+ *
+ *  La primera fila contiene la clave secreta. Las siguientes filas son los intentos del usuario.
+ *  Debido a esto, la tabla tiene un tamaño de [MAX_ATTEMPTS] + 1 filas * KEY_LENGTH columnas.
+ *
+ *  +---+---+---+---+
+ *  | 5 | 6 | 1 | 1 | <- Clave secreta
+ *  +---+---+---+---+
+ *  | 3 | 4 | 5 | 6 | <- Intento 1
+ *  +---+---+---+---+
+ *  | 1 | 5 | 2 | 2 | <- Intento 2
+ *  +---+---+---+---+
+ *  | 4 | 2 | 3 | 1 | <- ..Intento KEY_LENGTH
+ *  +---+---+---+---+
+ *
+ */
 typedef int table_t[MAX_ATTEMPTS + 1][KEY_LENGTH];
 
+/**
+ * Genera un número aleatorio entre [min] y [max].
+ *
+ * @param min El valor mínimo del rango.
+ * @param max El valor máximo del rango.
+ *
+ * @return Un número entre entre [min] y [max].
+ */
 int genRange(int min, int max)
 {
     return min + (rand() % max);
 }
 
-// TODO: try to make a generic formatter that works for both int* and char*
-// without casting pointers or using templates >_<.
-string formatIntArray(int *row)
-{
-    string out = "[";
-    for (int i = 0; i < KEY_LENGTH; i++)
-    {
-        out += to_string(row[i]);
-        if (i < KEY_LENGTH - 1)
-        {
-            out += ", ";
-        }
-    }
-    out += "]";
-    return out;
-}
-
-string formatClue(char *buf)
-{
-    string out = "[";
-    for (int i = 0; i < KEY_LENGTH; i++)
-    {
-        out += buf[i];
-        if (i < KEY_LENGTH - 1)
-        {
-            out += ", ";
-        }
-    }
-    out += "]";
-    return out;
-}
-
-void genSecretKey(table_t table)
-{
-    for (int col = 0; col < KEY_LENGTH; col++)
-    {
-        table[0][col] = genRange(KEY_RANGE_MIN, KEY_RANGE_MAX);
-    }
-
-    cout << "key: " << formatIntArray(table[0]) << endl;
-}
-
+/**
+ * Verifica si un arreglo contiene un valor, de manera lineal.
+ * @param buf El arreglo a verificar.
+ * @param val El valor a buscar.
+ * @return true si el valor existe en el arreglo, false en caso contrario.
+ */
 bool arrayContains(int *buf, int val)
 {
     for (int i = 0; i < KEY_LENGTH; i++)
@@ -87,7 +62,56 @@ bool arrayContains(int *buf, int val)
     return false;
 }
 
-bool getClue(table_t table, char *buf, int attempt)
+/// @brief  Formatea un arreglo de enteros.
+/// @tparam T El tipo de dato del arreglo, puede ser int o char.
+/// @param buf El arreglo a formatear.
+/// @return Una cadena de texto con el formato [1, 2, 3, n]
+template <typename T>
+string formatArray(T *buf)
+{
+    string out = "[";
+    for (int i = 0; i < KEY_LENGTH; i++)
+    {
+        typeid(T) == typeid(char) ? out += buf[i] : out += to_string(buf[i]);
+        if (i < KEY_LENGTH - 1)
+        {
+            out += ", ";
+        }
+    }
+    out += "]";
+    return out;
+}
+
+/**
+ * Genera una clave con digitos entre [KEY_RANGE_MIN] y [KEY_RANGE_MAX].
+ * Cada dígito de la clave es único, es decir, no se repite.
+ * @param table Referencia a la tabla donde se guardará la clave, en la primera fila.
+ */
+void genSecretKey(table_t table)
+{
+    int *row = table[0];
+    for (int col = 0; col < KEY_LENGTH; col++)
+    {
+        int dice = genRange(KEY_RANGE_MIN, KEY_RANGE_MAX);
+        // Si el número ya existe en la clave, se vuelve a generar otro número.
+        while (arrayContains(row, dice))
+        {
+            dice = genRange(KEY_RANGE_MIN, KEY_RANGE_MAX);
+        }
+        row[col] = dice;
+    }
+    cout << "[SECRET_KEY]: " << formatArray<int>(table[0]) << endl;
+}
+
+/**
+ * Actualiza la tabla con los intentos del usuario y genera las pistas.
+ *
+ * @param table Referencia a la tabla donde se almacenan los intentos del usuario.
+ * @param buf Referencia al arreglo donde se almacenarán las pistas.
+ * @param attempt El número de intento actual.
+ * @return true si el usuario adivinó la clave, false en caso contrario.
+ */
+bool updateTable(table_t table, char *buf, int attempt)
 {
     int *key = table[0];
 
@@ -105,17 +129,29 @@ bool getClue(table_t table, char *buf, int attempt)
     return hits == KEY_LENGTH;
 }
 
-bool checkInput(int input)
+/**
+ * Verifica si el dígito ingresado por el usuario es válido,
+ * es decir, si está dentro del rango de números de la clave.
+ * @param input El dígito ingresado por el usuario.
+ * @return Si el dígito es válido o no.
+ */
+bool verifyInput(int input)
 {
     return input >= KEY_RANGE_MIN && input <= KEY_RANGE_MAX;
 }
 
+/**
+ * Lee un dígito ingresado por el usuario, y lo valida.
+ * @param input Referencia al entero donde se almacenará el dígito ingresado.
+ * @param attempt El número de intento actual.
+ * @param index El índice del dígito actual de [KEY_LENGTH].
+ */
 void readDigit(int *input, int attempt, int index)
 {
     while (true)
     {
         cout << "Ingrese el valor " << (index + 1) << "/" << KEY_LENGTH << ": ";
-        if (!(cin >> *input) || !checkInput(*input))
+        if (!(cin >> *input) || !verifyInput(*input))
         {
             cout << "Valor inválido. Ingrese un número entre " << KEY_RANGE_MIN << " y " << KEY_RANGE_MAX << endl;
             cin.clear();
@@ -125,7 +161,13 @@ void readDigit(int *input, int attempt, int index)
         break;
     }
 }
-void updateTable(table_t table, int attempt)
+
+/**
+ * Lee la entrada del usuario y la almacena en la tabla.
+ * @param table Referencia a la tabla donde se almacenará la entrada.
+ * @param attempt El número de intento actual.
+ */
+void readInput(table_t table, int attempt)
 {
     for (int i = 0; i < KEY_LENGTH; i++)
     {
@@ -135,16 +177,21 @@ void updateTable(table_t table, int attempt)
     }
 }
 
+/**
+ * Muestra los intentos del usuario y las pistas.
+ * Además, verifica si el usuario adivinó la clave.
+ * @param table Referencia a la tabla donde se almacenan los intentos del usuario.
+ * @param attempt El número de intento actual.
+ */
 bool showAttempts(table_t table, int attempt)
 {
     for (int row = 0; row < attempt; row++)
     {
         char clue[KEY_LENGTH];
-        bool hitAll = getClue(table, clue, row + 1);
+        bool hitAll = updateTable(table, clue, row + 1);
         cout << "Intento " << row + 1 << "/" << MAX_ATTEMPTS << " - "
-             << "Entrada: " << formatIntArray(table[row + 1])
-             << " - Salida: " << formatClue(clue) << endl;
-
+             << "Entrada: " << formatArray<int>(table[row + 1])
+             << " - Salida: " << formatArray<char>(clue) << endl;
         if (hitAll)
         {
             cout << "Ganaste!" << endl;
@@ -155,13 +202,21 @@ bool showAttempts(table_t table, int attempt)
     return true;
 }
 
+/**
+ * Ejecuta el `main loop` del programa.
+ *
+ * Los intentos incrementan a medida que el usuario ingresa sus valores.
+ * Las condiciones contempladas para terminar el programa son:
+ *  - El usuario adivina la clave.
+ *  - El usuario se queda sin intentos.
+ */
 int run(table_t table)
 {
     int attempt = 1;
     while (attempt <= MAX_ATTEMPTS)
     {
         cout << " --- Intento " << attempt << "/" << MAX_ATTEMPTS << " --- " << endl;
-        updateTable(table, attempt);
+        readInput(table, attempt);
 
         bool shouldContinue = showAttempts(table, attempt);
 
@@ -180,10 +235,12 @@ int main(int argc, char const *argv[])
     cout << "Bienvenido/a al Master Mind!" << endl;
     cout << "Adivina la clave de 4 digitos. Tienes " << MAX_ATTEMPTS << " intentos." << endl;
     srand(time(NULL));
-
     table_t table_t;
+    // 1) Generar clave secreta
     genSecretKey(table_t);
+    // 2) Ejecutar el main loop
     int score = run(table_t);
+    // 3) Mostrar puntaje final
     std::cout << "Tu puntaje es: " << score << "/" << MAX_ATTEMPTS << std::endl;
     return 0;
 }
